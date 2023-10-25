3d6031003d24310ce6446d84e6c9c906a1f05a70
Lodrick <dev.frenchlod@gmail.com>
2023-10-25 15:56:30 +0200

    ✅ Home add img and small style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <HomeStyled>Home</HomeStyled>;
 };
 
 export default Home;
+const HomeStyled = styled.div`
+  height: 100vh;
+  background: url("./items/homenoconnect.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
