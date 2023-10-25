418eeb7b44c5347a56acc391af6b43358422308a
Lodrick <dev.frenchlod@gmail.com>
2023-10-25 16:56:57 +0200

    🔥 remove code to re begin

---
+++ src/components/modalAccueil/StatutUser.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const StatutUser = () => {
+  return <div>StatutUser</div>;
+};
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { DynamicContextProvider } from "./context/DynamicContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DynamicContextProvider>
+        <App />
+      </DynamicContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
