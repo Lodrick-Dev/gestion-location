4d49c27885567be5fed8cee201bb984d2c5112ab
Lodrick <dev.frenchlod@gmail.com>
2023-10-25 15:49:25 +0200

    🚧 Prepare App and Index to navigation RRDom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
