dd869a2a875cbe32d30df519cb7350c2751c3030
Lodrick <dev.frenchlod@gmail.com>
2023-11-12 15:29:34 +0100

    🎨 other code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { DynamicContextProvider } from "./context/DynamicContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
